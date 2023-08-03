cd4dabc2282f630ff429eeacda74ceed6b76c2c3
Miika Alikirri <miika.alikirri@suse.com>
2023-08-03 11:35:17 +0300

    Added deps to make tests runnable

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   roots: ['<rootDir>'],
   transform: {
     '\\.(js|jsx)?$': 'babel-jest'
@@ -11,7 +11,7 @@ module.exports = {
     // avoid errors like "Unexpected token ." See https://stackoverflow.com/a/54646930
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
   },
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/pkg/'],
   setupFilesAfterEnv: [
     '@testing-library/jest-dom/extend-expect',
   ],
