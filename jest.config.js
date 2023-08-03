d5ad234be1a1c2d31dd2350dc8b67806a235e060
Miika Alikirri <miika.alikirri@suse.com>
2023-08-03 16:43:07 +0300

    more dependency updates and test seem to run somewhat correctly

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 export default {
   roots: ['<rootDir>'],
+  testEnvironment: "jsdom",
   transform: {
     '\\.(js|jsx)?$': 'babel-jest'
   },
@@ -11,6 +12,7 @@ export default {
     // avoid errors like "Unexpected token ." See https://stackoverflow.com/a/54646930
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
   },
+  transformIgnorePatterns: ['/node_modules/(?!(\@patternfly)/)'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/pkg/'],
   setupFilesAfterEnv: [
     '@testing-library/jest-dom/extend-expect',
